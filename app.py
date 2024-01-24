c18b6d0b51f5047f76448d419ffd805c64cbdce6
Francesco <francescofinn109@gmail.com>
2024-01-24 16:56:39 -0500

    Updated app.py to display leaderboard ranking

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 # Francesco Finn & Emilio Munguia
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
+def get_data():
+    conn = sqlite3.connect('starwars.db')
+    cur = conn.cursor()
+    cur.execute("SELECT name, elo FROM characters ORDER BY elo")
+    data = cur.fetchall()
+    cur.close()
+    conn.close()
+    return data
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    data = get_data()
+    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
