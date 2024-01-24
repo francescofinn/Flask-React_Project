9e6de254f23b95004886956b34a49f2280b62329
Emilio Wagner <emiliomunguia04@gmail.com>
2024-01-24 18:59:31 -0500

    integrated elo changes when voting

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 # Francesco Finn & Emilio Munguia
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 import random
 import sqlite3
+import elo
 
 app = Flask(__name__)
+
 
 def get_data():
     conn = sqlite3.connect('starwars.db')
@@ -14,26 +16,67 @@ def get_data():
     conn.close()
     return data
 
+
 def get_character_pair():
     c1 = random.randint(1, 82)
     c2 = 0
 
     while c1 == c2 or c2 == 0:
-        c2 = random.randint(1, 82) 
-    
+        c2 = random.randint(1, 82)
+
     conn = sqlite3.connect('starwars.db')
     cur = conn.cursor()
-    cur.execute("SELECT name FROM characters WHERE rowid = " + str(c1) + " OR rowid = " + str(c2))
+    cur.execute("SELECT name FROM characters WHERE rowid = " +
+                str(c1) + " OR rowid = " + str(c2))
     pair_data = cur.fetchall()
     cur.close()
     conn.close()
     return pair_data
 
-@app.route('/')
+
+def get_elo(name):
+    conn = sqlite3.connect('starwars.db')
+    cur = conn.cursor()
+    cur.execute("SELECT elo FROM characters WHERE name = ?", (name,))
+    elo = cur.fetchone()
+    conn.close()
+    return elo[0]
+
+
+@app.route('/update_elo', methods=['POST'])
+def update_elo():
+    data = request.get.json()
+    winner = data['winner']
+    loser = data['loser']
+
+    winner_elo = get_elo(winner)
+    loser_elo = get_elo(loser)
+
+    result = elo.EloRating(winner_elo, loser_elo, 30, 1)
+
+    winner_elo = result[0]
+    loser_elo = result[1]
+
+    conn = sqlite3.connect('starwars.db')
+    cur = conn.cursor()
+
+    cur.execute("UPDATE characters SET elo = ? WHERE name = ?",
+                (winner_elo, winner))
+    cur.execute("UPDATE characters SET elo = ? WHERE name = ?",
+                (loser_elo, loser))
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return jsonify({'success': True})
+
+
 def index():
     data = get_data()
     pair_data = get_character_pair()
     return render_template('index.html', data=data, pair_data=pair_data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- elo.py
+++ elo.py
@@ -6,7 +6,7 @@ import math
 
 def Probability(rating1, rating2):
 
-	return 1.0 * 1.0 / (1 + 1.0 * math.pow(10, 1.0 * (rating1 - rating2) / 400))
+    return 1.0 * 1.0 / (1 + 1.0 * math.pow(10, 1.0 * (rating1 - rating2) / 400))
 
 
 # Function to calculate Elo rating
@@ -15,29 +15,24 @@ def Probability(rating1, rating2):
 # Player A wins or Player B.
 def EloRating(Ra, Rb, K, d):
 
-	# To calculate the Winning
-	# Probability of Player B
-	Pb = Probability(Ra, Rb)
+    # To calculate the Winning
+    # Probability of Player B
+    Pb = Probability(Ra, Rb)
 
-	# To calculate the Winning
-	# Probability of Player A
-	Pa = Probability(Rb, Ra)
+    # To calculate the Winning
+    # Probability of Player A
+    Pa = Probability(Rb, Ra)
 
-	# Case -1 When Player A wins
-	# Updating the Elo Ratings
-	if (d == 1):
-		Ra = Ra + K * (1 - Pa)
-		Rb = Rb + K * (0 - Pb)
+    # Case -1 When Player A wins
+    # Updating the Elo Ratings
+    if (d == 1):
+        Ra = Ra + K * (1 - Pa)
+        Rb = Rb + K * (0 - Pb)
 
-	# Case -2 When Player B wins
-	# Updating the Elo Ratings
-	else:
-		Ra = Ra + K * (0 - Pa)
-		Rb = Rb + K * (1 - Pb)
+    # Case -2 When Player B wins
+    # Updating the Elo Ratings
+    else:
+        Ra = Ra + K * (0 - Pa)
+        Rb = Rb + K * (1 - Pb)
 
-	print("Updated Ratings:-")
-	print("Ra =", round(Ra, 6), " Rb =", round(Rb, 6))
-
-
-
-
+    return (round(Ra), round(Rb))
